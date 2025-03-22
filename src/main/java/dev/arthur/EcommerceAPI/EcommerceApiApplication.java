4e7d6675ce956a13ef32470a89db04cf758d2d75
Arthur Bassani <arthursilvabassani@gmail.com>
2025-03-22 03:36:03 -0300

    feat: Add dotenv in project

--- src/main/java/dev/arthur/EcommerceAPI/EcommerceApiApplication.java
+++ src/main/java/dev/arthur/EcommerceAPI/EcommerceApiApplication.java
@@ -1,5 +1,6 @@
 package dev.arthur.EcommerceAPI;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommerceApiApplication.class, args);
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+        SpringApplication.run(EcommerceApiApplication.class, args);
 	}
 
 }
