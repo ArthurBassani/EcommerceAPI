a0f1ee4b44a5ba9ae8cd009dae89aa9c1e32342d
Arthur Bassani <arthursilvabassani@gmail.com>
2025-03-28 13:58:40 -0300

    feat: add columns created_at and updated_at migration

---
+++ src/main/resources/db/migration/V028__add_column_updated_at_tables.sql
@@ -0,0 +1,49 @@
+ALTER TABLE
+    "users"
+ADD
+    "updated_at" TIMESTAMP NULL;
+
+ALTER TABLE
+    "states"
+ADD
+    "updated_at" TIMESTAMP NULL;
+
+ALTER TABLE
+    "cities"
+ADD
+    "updated_at" TIMESTAMP NULL;
+
+ALTER TABLE
+    "addresses"
+ADD
+    "updated_at" TIMESTAMP NULL;
+
+ALTER TABLE
+    "carts"
+ADD
+    "updated_at" TIMESTAMP NULL;
+
+ALTER TABLE
+    "favorites"
+ADD
+    "updated_at" TIMESTAMP NULL;
+
+ALTER TABLE
+    "products"
+ADD
+    "updated_at" TIMESTAMP NULL;
+
+ALTER TABLE
+    "roles"
+ADD
+    "updated_at" TIMESTAMP NULL;
+
+ALTER TABLE
+    "sales"
+ADD
+    "updated_at" TIMESTAMP NULL;
+
+ALTER TABLE
+    "shopping"
+ADD
+    "updated_at" TIMESTAMP NULL;
